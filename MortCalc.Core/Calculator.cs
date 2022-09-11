14bc8e1e4708815cec01c0a01548092d08100296
pingzing <mcalistern@gmail.com>
2022-09-11 17:16:36 +0300

    Add project files.

---
+++ MortCalc.Core/Calculator.cs
@@ -0,0 +1,84 @@
+﻿namespace MortCalc.Core
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// The percentage at which the ASP subsidy kicks in. Currently 3.8%.
+        /// </summary>
+        private const decimal AspInterestSoftCap = .038m;
+
+        /// <summary>
+        /// For any interest that exceeds the soft cap, this is the percentage of it that will be paid by the subsidy. Currently 70%.
+        /// </summary>
+        private const decimal AspSubsidyPercentage = 0.7m;
+
+        public record MonthlyPayment(
+            decimal AmountPerMonth,
+            decimal PricipalPortion,
+            decimal InterestPortion,
+            decimal AspSavings
+        );
+
+        public static MonthlyPayment CalculateMonthlyPayment(
+            decimal annualInterestRate,
+            uint numPayments,
+            decimal principal,
+            bool useAspSubsidy
+        )
+        {
+            decimal monthly = annualInterestRate / 12;
+            decimal monthlyPaymentNoInterest = principal / numPayments;
+
+            if (monthly == 0)
+            {
+                return new MonthlyPayment(monthlyPaymentNoInterest, monthlyPaymentNoInterest, 0, 0);
+            }
+
+            decimal onePlusRToTheN = (decimal)Math.Pow(1 + (double)monthly, numPayments);
+
+            if (useAspSubsidy && annualInterestRate > AspInterestSoftCap)
+            {
+                decimal monthlySoftCapInterest = AspInterestSoftCap / 12;
+                decimal onePlusRToTheNSoftCap = (decimal)
+                    Math.Pow(1 + (double)monthlySoftCapInterest, numPayments);
+
+                decimal monthlyPaymentAtSoftCap =
+                    principal
+                    * (
+                        (monthlySoftCapInterest * onePlusRToTheNSoftCap)
+                        / (onePlusRToTheNSoftCap - 1)
+                    );
+
+                decimal monthlyPaymentWithNoSubidy =
+                    principal * ((monthly * onePlusRToTheN) / (onePlusRToTheN - 1));
+
+                decimal preSoftCapInterestAmount =
+                    monthlyPaymentAtSoftCap - monthlyPaymentNoInterest;
+                decimal interestAmountWithNoSubsidy =
+                    monthlyPaymentWithNoSubidy - monthlyPaymentNoInterest;
+
+                decimal amountCoveredBySubsidy =
+                    interestAmountWithNoSubsidy - preSoftCapInterestAmount;
+                decimal monthlyPaymentWithSubsidy =
+                    monthlyPaymentAtSoftCap + (amountCoveredBySubsidy * (1 - AspSubsidyPercentage));
+
+                return new MonthlyPayment(
+                    monthlyPaymentWithSubsidy,
+                    monthlyPaymentNoInterest,
+                    (monthlyPaymentWithSubsidy - monthlyPaymentNoInterest),
+                    (amountCoveredBySubsidy * AspSubsidyPercentage)
+                );
+            }
+
+            decimal amountPerMonth =
+                principal * ((monthly * onePlusRToTheN) / (onePlusRToTheN - 1));
+
+            return new MonthlyPayment(
+                amountPerMonth,
+                monthlyPaymentNoInterest,
+                amountPerMonth - monthlyPaymentNoInterest,
+                0
+            );
+        }
+    }
+}
---
+++ MortCalc.Tool/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CommandLine;
+using static MortCalc.Core.Calculator;
+
+namespace MortCalc.Tool;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var interestRateOption = new Option<decimal>(
+            aliases: new[] { "--interest", "-i" },
+            getDefaultValue: () => 0.5m,
+            description: "Your mortgage's interest annual rate, as a percentage from 0.0 to 100.0."
+        );
+
+        var numPaymentsOption = new Option<uint>(
+            aliases: new[] { "--payments", "-p" },
+            getDefaultValue: () => 240,
+            description: "The number of total payments in your mortgage. Usually the number of years, divided by 12 (i.e. total number of months)."
+        );
+
+        var loanPrincipalOption = new Option<decimal>(
+            aliases: new[] { "--amount", "-a" },
+            getDefaultValue: () => 161_000m,
+            description: "Your total ammount borrowed in your loan, also known as its principal."
+        );
+
+        var rootCommand = new RootCommand(
+            "Calculate your monthly mortgage payment. With optional Finnish ASP subsidy calculations!"
+        );
+
+        rootCommand.AddOption(interestRateOption);
+        rootCommand.AddOption(numPaymentsOption);
+        rootCommand.AddOption(loanPrincipalOption);
+
+        rootCommand.SetHandler(
+            PrintMortgage,
+            interestRateOption,
+            numPaymentsOption,
+            loanPrincipalOption
+        );
+
+        rootCommand.Invoke(args);
+    }
+
+    private static void PrintMortgage(decimal interestRate, uint numPayments, decimal loanPrincipal)
+    {
+        var monthlyPayment = CalculateMonthlyPayment(
+            interestRate / 100m,
+            numPayments,
+            loanPrincipal,
+            true
+        );
+
+        string amtPerMonth = $"€{monthlyPayment.AmountPerMonth:0.00}";
+        string principal = $"€{monthlyPayment.PricipalPortion:0.00}";
+        string interest = $"€{monthlyPayment.InterestPortion:0.00}";
+        string aspSavings = $"€{monthlyPayment.AspSavings:0.00}";
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine(
+            $"{"Monthly Payment", 15}{"Principal", 15}{"Interest", 15}{"ASP Savings", 15}"
+        );
+        Console.WriteLine("____________________________________________________________");
+        Console.WriteLine(
+            $"{amtPerMonth, 15}" + $"{principal, 15}" + $"{interest, 15}" + $"{aspSavings, 15}"
+        );
+    }
+}
